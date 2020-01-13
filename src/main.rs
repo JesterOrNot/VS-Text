1f2bf714b58e014ea0e54078f866aace9991d8c3
Sean Hellum <seanhellum45@gmail.com>
2020-01-13 18:42:34 +0000

    initialize some critical functions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+    initialize_screen();
+    hide_cursor();
+    std::thread::sleep(100 * std::time::Duration::from_millis(20));
+    show_cursor();
+}
+fn initialize_screen() {
+    println!("\x1b[2J");
+}
+fn hide_cursor() {
+    println!("\x1b[?25l");
+}
+fn show_cursor() {
+    println!("\x1b[?25h");
 }
