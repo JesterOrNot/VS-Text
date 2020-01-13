370ab2586a7a7dfcc30aefd6d0708891443a0562
Sean Hellum <seanhellum45@gmail.com>
2020-01-13 19:35:20 +0000

    update

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,25 @@
 use terminal_size::terminal_size;
 fn main() {
     clear_screen();
-    print_border_top();
     hide_cursor();
-    std::thread::sleep(100 * std::time::Duration::from_millis(20));
+    print_border();
+    std::thread::sleep(100 * std::time::Duration::from_millis(30));
     show_cursor();
 }
-fn print_border_top() {
+fn print_border() {
     let (width, height) = terminal_size().unwrap();
-    println!("\x1b[0;0f");
+    // TOP
+    print!("\x1b[0;0f");
+    print!("\x1b[s");
     for i in 0..width.0 {
-        print!("-\x1b[1C");
+        print!("_\x1b[1C");
     }
-    for j in 0..height.0 {
-        print!("\x1b[1B|");
+    // Left and right
+    print!("\x1b[1B\x1b[1000D|\x1b[1000C");
+    for j in 1..height.0 {
+        print!("|\x1b[1B\x1b[999D|\x1b[999C");
     }
-    println!("\x1b[0;0f");
+    print!("\x1b[10000B|");
 }
 fn clear_screen() {
     println!("\x1b[2J");
