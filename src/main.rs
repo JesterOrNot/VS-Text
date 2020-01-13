8b7678843309b545024ffd5d6e506fe7c607f760
Sean Hellum <seanhellum45@gmail.com>
2020-01-13 19:00:21 +0000

    start the border

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,23 @@
+use terminal_size::terminal_size;
 fn main() {
-    initialize_screen();
+    clear_screen();
+    print_border_top();
     hide_cursor();
     std::thread::sleep(100 * std::time::Duration::from_millis(20));
     show_cursor();
 }
-fn initialize_screen() {
+fn print_border_top() {
+    let (width, height) = terminal_size().unwrap();
+    println!("\x1b[0;0f");
+    for i in 0..width.0 {
+        print!("-\x1b[1C");
+    }
+    for j in 0..height.0 {
+        print!("\x1b[1B|");
+    }
+    println!("\x1b[0;0f");
+}
+fn clear_screen() {
     println!("\x1b[2J");
 }
 fn hide_cursor() {
