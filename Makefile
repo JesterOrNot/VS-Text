78998f7b1e807a13eed2a48710a4f13592d77509
Sean Hellum <seanhellum45@gmail.com>
2020-01-14 02:09:58 +0000

    re initialize project

---
+++ Makefile
@@ -0,0 +1,2 @@
+vstext: src/main.cpp
+	g++ -o vstext src/main.cpp -Wall -Wextra -pedantic
---
+++ src/main.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+int main() {
+  std::cout << "Hello World From C++!\n";
+  return 0;
+}
