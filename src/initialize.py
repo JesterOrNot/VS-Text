dad9f93061f96356430a5edbf89ab9bfb2268db5
Sean Hellum <seanhellum45@gmail.com>
2020-01-15 19:07:47 +0000

    update

--- src/initialize.py
+++ src/initialize.py
@@ -1,2 +1,8 @@
-import libtmux
-libtmu
+import libtmux, time
+server = libtmux.Server()
+session = server.new_session(session_name="c", )
+window = session.attached_window
+pane = window.split_window(attach=True)
+pane.send_keys("ls")
+time.sleep(20)
+session.kill_session()
